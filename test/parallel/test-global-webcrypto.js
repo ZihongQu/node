a6149206cc55eb2642c0c5010cb55fc70d2ddb4b
ZihongQu <zihongqu@gmail.com>
2024-05-16 20:44:44 +0930

    tools: move webcrypto into no-restricted-properties
    Since eslint fixed https://github.com/eslint/eslint/issues/16412 and we
    are on eslint v8.57.0 so that we can take advantage of
    no-restricted-properties rule for webcrypto.

--- test/parallel/test-global-webcrypto.js
+++ test/parallel/test-global-webcrypto.js
@@ -7,7 +7,8 @@ if (!common.hasCrypto)
 const assert = require('assert');
 const crypto = require('crypto');
 
-/* eslint-disable no-restricted-syntax */
+/* eslint-disable no-restricted-properties, no-restricted-syntax */
+// TODO(zi): Remove no-restricted-syntax once we add no-restricted-properties to test/.eslintrc.yaml
 assert.strictEqual(globalThis.crypto, crypto.webcrypto);
 assert.strictEqual(Crypto, crypto.webcrypto.constructor);
 assert.strictEqual(SubtleCrypto, crypto.webcrypto.subtle.constructor);
